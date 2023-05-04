7da2f5528f56c1af487691931dad69b7e23127b5
ChenXiaoYingCaryn <739130811@qq.com>
2023-05-04 23:51:04 +0800

    first commit

---
+++ src/main/java/com/example/redisdemo/config/LettuceRedisConfig.java
@@ -0,0 +1,5 @@
+package com.example.redisdemo.config;/**
+ * @author xiaoying
+ * @create 2023/5/3 11:57
+ */public class LettuceRedisConfig {
+}
---
+++ src/main/java/com/example/redisdemo/controller/LettuceTest.java
@@ -0,0 +1,5 @@
+package com.example.redisdemo.controller;/**
+ * @author xiaoying
+ * @create 2023/5/4 10:16
+ */public class LettuceTest {
+}
